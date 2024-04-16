a4f6ddc9a3a213cef4dd14e0d8cb513c718a77ae
Rohan Julka <rohanjulka19@gmail.com>
2024-04-16 03:26:23 +0100

    s3 fix

--- drive/integrations.py
+++ drive/integrations.py
@@ -4,6 +4,7 @@ import boto3
 import botocore
 
 
+
 aws_secret_access_key = "SaTW+yvnFTvFXC7rWZMKuSbWQjzZWvkobyaU3f4y"  
 class IntegrationException(Exception):
     def __init__(self, error=None, message="Integration Error"):
